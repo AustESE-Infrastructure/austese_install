9244b8920c217e1e20ff3026c4c04ccf22c2439c
desmond <desmond.allan.schmidt@gmail.com>
2013-12-10 18:27:38 +1000

    modified php installer for ubuntu 12.04

--- install/php5.sh
+++ install/php5.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 . ./functions/global.sh
+php_mod_enable() {
+  appendto "/etc/php5/apache2/php.ini" "extension=$1.so"
+  appendto "/etc/php5/cli/php.ini" "extension=$1.so"
+}
+PHP5ENMOD=`which php5enmod`
+ENABLE_MOD=
+if [ -z "$PHP5ENMOD" ]; then
+  ENABLE_MOD=$PHP5ENMOD
+else
+  ENABLE_MOD=php_mod_enable
+fi
 #php5 itself
 PHP5=`which php5`
 if [ -z "$PHP5" ]; then
@@ -8,32 +19,32 @@ fi
 PHP5XSL=`php5 -m | grep xsl`
 if [ -z "$PHP5XSL" ]; then
   ensure apt-get -qq install php5-xsl
-  php5enmod xsl
+  $ENABLE_MOD xsl
 fi
 PHP5CURL=`php5 -m | grep curl`
 if [ -z "$PHP5CURL" ]; then
   ensure apt-get -qq install php5-curl
-  php5enmod curl
+  $ENABLE_MOD curl
 fi
 IMAGICK=`php5 -m | grep imagick`
 if [ -z "$IMAGICK" ]; then
   ensure apt-get -qq install php5-imagick
-  php5enmod imagick
+  $ENABLE_MOD imagick
 fi
 MONGO=`php5 -m | grep mongo`
 if [ -z "$MONGO" ]; then
   ensure apt-get -q -y install php5-mongo
-  php5enmod mongo
+  $ENABLE_MOD mongo
 fi
 MYSQL=`php5 -m | grep mysql`
 if [ -z "$MYSQL" ]; then
   ensure apt-get -qq install php5-mysql
-  php5enmod mysql
+  $ENABLE_MOD mysql
 fi
 PHP5GD=`php5 -m | grep gd`
 if [ -z "$PHP5GD" ]; then
   ensure apt-get -qq install php5-gd
-  php5enmod gd
+  $ENABLE_MOD gd
 fi
 if [ ! -d "/usr/include/php5" ]; then
   ensure apt-get -qq install php5-dev

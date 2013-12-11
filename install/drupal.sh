a87b0d31deaaab8cb3d7d131306caf31dfb0e66f
schmidda <desmond.schmidt@mail.com>
2013-12-11 14:52:44 +1000

    Fixed drupal.sh - now updates password correctly

--- install/drupal.sh
+++ install/drupal.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 . ./functions/global.sh
+PASSWORD=$1
+if [ -z "$PASSWORD" ]; then
+  echo "password is empty: install/drupal.sh"
+  exit 1
+fi
 WEBROOT=`./get/webroot.sh`
 ANSWER="n"
 if [ -d "$WEBROOT/austese/" ]; then
@@ -11,7 +16,21 @@ elif [ -d "$WEBROOT/austese" ]; then
   echo "Exiting..."
   exit 1
 fi
+SETTINGS="$WEBROOT/austese/sites/default/settings.php"
 ensure cat ./objects/austese.tar.gz.* | tar xz -C "$WEBROOT"
+ensure rm -f /tmp/settings.php
+sed "s/'username' => 'austese',/'username' => 'root',/" <$SETTINGS >/tmp/settings.php
+if [ $? -ne 0 ]; then
+  echo "sed (1) failed on settings.php"
+  exit 1
+fi
+ensure chmod 777 $SETTINGS
+sed "s/'password' => 'austese9875\!',/'password' => '$PASSWORD',/" </tmp/settings.php >$SETTINGS
+if [ $? -ne 0 ]; then
+  echo "sed (2) failed on settings.php"
+  exit 1
+fi
+ensure chmod 644 $SETTINGS
 ensure chown -R www-data "$WEBROOT/austese"
 ensure chgrp -R www-data "$WEBROOT/austese"
 exit 0

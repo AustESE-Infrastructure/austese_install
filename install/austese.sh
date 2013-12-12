cc96b92cd13be8bf153559127908a0494a7a4bbb
schmidda <desmond.schmidt@mail.com>
2013-12-12 10:13:41 +1000

    Fixed austese.sh script. It now creates the austese user and grants all privileges.

--- install/austese.sh
+++ install/austese.sh
@@ -5,12 +5,16 @@ AUSTESEDB=`echo 'show databases' | mysql --user=root --password=jabberw0cky -B |
 if [ ! -z "$AUSTESEDB" ]; then
   read -p "Erase existing austese database?(y/n) " ANSWER
   if [ "$ANSWER" = "y" ]; then
-    mysqladmin -f -u root -p$PASSWORD drop austese
+    ensure mysqladmin -f -u root -p$PASSWORD drop austese
+    ensure mysqladmin -u root -p$PASSWORD create austese
+    ensure mysql -u root -p$PASSWORD austese < ./objects/austese.sql
   else
     echo "Exiting..."
     exit 1
   fi
 fi
-mysqladmin -u root -p$PASSWORD create austese
-ensure mysql -u root -p$PASSWORD austese < ./objects/austese_all.sql
+mysql -u root -p$PASSWORD -e "delete from mysql.user where user='austese';"
+mysql -u root -p$PASSWORD -e "flush privileges;"
+mysql -u root -p$PASSWORD -e "CREATE USER 'austese'@'localhost' IDENTIFIED BY '$PASSWORD';"
+mysql -u root -p$PASSWORD -e "grant all privileges on *.* to 'austese'@'localhost';"
 exit 0

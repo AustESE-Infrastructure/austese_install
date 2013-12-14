8cf11728e52ed0c4fcece336204f961dd09262ad
desmond <desmond@gabriel.(none)>
2013-12-14 12:30:26 +1000

    added back calliope.sh

---
+++ install/calliope.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+. ./functions/global.sh
+PASSWORD=$1
+if [ -z "$PASSWORD" ]; then
+  echo "PASSWORD to calliope.sh cannot be empty"
+  exit 1
+fi
+CATALINA_BASE=`./get/catalina_base.sh`
+CATALINA_HOME=`./get/catalina_home.sh`
+WADR="$CATALINA_BASE/webapps"
+WUSR=`./get/apacheuser.sh`
+WGRP=`./get/apachegroup.sh`
+if [ -z "$WADR" ] || [ ! -d "$WADR" ]; then
+  echo "webapps dir not found"
+  exit 1
+fi
+TBIN="$CATALINA_HOME/bin"
+if [ -z "$TBIN" ] || [ ! -d "$TBIN" ]; then
+  echo "tomcat bin dir not found"
+  exit 1
+fi
+<<<<<<< HEAD
+ensure cp ./objects/calliope.war $WADR
+ensure chown tomcat7 $TBIN/calliope.war
+ensure chgrp tomcat7 $TBIN/calliope.war
+=======
+# embed users password into calliope web.xml
+ensure rm -rf /tmp/calliope
+ensure mkdir /tmp/calliope
+ensure cp ./objects/calliope.war /tmp/calliope
+CPWD=`pwd`
+ensure cd /tmp/calliope
+ensure jar xf calliope.war
+ensure cd ..
+ensure sed -i -e "s/jabberw0cky/$PASSWORD/" /tmp/calliope/WEB-INF/web.xml
+jar cf calliope.war -C calliope WEB-INF
+cd "$CPWD"
+ensure cp /tmp/calliope.war $WADR
+>>>>>>> 4cf8154eb625271b9f8c8ff9da099dc7549e8820
+ensure cp ./objects/LibPath.class $TBIN
+ensure cp ./objects/setenv.sh $TBIN
+exit 0
